2819fdc8e2f54a3fa7252ca12898b3be95ff7e5a
Mariana Muñoz <167018536+ProyecMar26@users.noreply.github.com>
2024-08-13 08:28:45 -0500

    Update consultSalesOrders.php

--- php/sales/consultSalesOrders.php
+++ php/sales/consultSalesOrders.php
@@ -1,5 +1,15 @@
 <?php
+require "../php/conn.php"; // Incluye la conexión a la base de datos
+
+// Verifica si la conexión fue exitosa
+if ($conn->connect_error) {
+    die("Conexión fallida: " . $conn->connect_error);
+}
+
 // Consulta de órdenes de venta existentes
 $sql_ordenes_venta = "SELECT * FROM ordenes_venta ORDER BY id_orden ASC";
 $resultado_ordenes_venta = $conn->query($sql_ordenes_venta);
+
+// Cierra la conexión a la base de datos
+$conn->close();
 ?>
