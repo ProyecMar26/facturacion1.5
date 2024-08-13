90836fc1a4e1e73ba1ab5711ebb122f04608e240
Mariana Muñoz <167018536+ProyecMar26@users.noreply.github.com>
2024-08-13 08:27:03 -0500

    Update getProducts.php

--- php/products/getProducts.php
+++ php/products/getProducts.php
@@ -1,11 +1,18 @@
 <?php
 function get_products($conn, $search_query) {
+    $search_query = $conn->real_escape_string($search_query);
     if ($search_query === '') {
         $sql_productos = "SELECT * FROM producto ORDER BY id_categoria, id_productos";
     } else {
         $sql_productos = "SELECT * FROM producto WHERE nombre LIKE '%$search_query%' ORDER BY id_categoria, id_productos";
     }
     $resultado_productos = $conn->query($sql_productos);
+
+    if (!$resultado_productos) {
+        die("Error en la consulta: " . $conn->error);
+    }
+
     return $resultado_productos;
 }
+
 ?>
